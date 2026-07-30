b3261a9d2baa5d8e240dbbe36a2e5aa3637fe6fc
med-40 <salahbel9140@gmail.com>
2026-07-30 23:03:12 +0100

    Create database session

---
+++ app/database/session.py
@@ -0,0 +1,19 @@
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+
+
+DATABASE_URL = "sqlite:///fleet_assets.db"
+
+
+engine = create_engine(
+    DATABASE_URL,
+    echo=False,
+    future=True
+)
+
+
+SessionLocal = sessionmaker(
+    bind=engine,
+    autoflush=False,
+    autocommit=False
+)

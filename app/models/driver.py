feba8dedea52c614e74c9586b9fc217f5f8037c0
med-40 <salahbel9140@gmail.com>
2026-07-30 23:29:19 +0100

    Create driver model

---
+++ app/models/driver.py
@@ -0,0 +1,44 @@
+from sqlalchemy import Column
+from sqlalchemy import Date
+from sqlalchemy import Integer
+from sqlalchemy import String
+
+from app.database.base import Base
+
+
+class Driver(Base):
+
+    __tablename__ = "drivers"
+
+    id = Column(
+        Integer,
+        primary_key=True
+    )
+
+    first_name = Column(
+        String(100),
+        nullable=False
+    )
+
+    last_name = Column(
+        String(100),
+        nullable=False
+    )
+
+    phone = Column(
+        String(50)
+    )
+
+    license_number = Column(
+        String(100),
+        unique=True
+    )
+
+    license_expiry_date = Column(
+        Date
+    )
+
+    status = Column(
+        String(50),
+        default="Active"
+    )

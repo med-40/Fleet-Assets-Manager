42ceb312704c04250b082e98d3cb79d1ddc2eec9
med-40 <salahbel9140@gmail.com>
2026-07-30 23:47:56 +0100

    Import all database models

---
+++ app/models/__init__.py
@@ -0,0 +1,35 @@
+from app.models.organization import Organization
+from app.models.branch import Branch
+from app.models.department import Department
+
+from app.models.role import Role
+from app.models.permission import Permission
+from app.models.user import User
+from app.models.user_role import UserRole
+
+from app.models.audit_log import AuditLog
+from app.models.setting import Setting
+
+from app.models.equipment_type import EquipmentType
+from app.models.equipment import Equipment
+from app.models.equipment_status import EquipmentStatus
+
+from app.models.driver import Driver
+from app.models.equipment_assignment import EquipmentAssignment
+
+from app.models.maintenance_type import MaintenanceType
+from app.models.maintenance_order import MaintenanceOrder
+from app.models.maintenance_schedule import MaintenanceSchedule
+
+from app.models.part_type import PartType
+from app.models.part import Part
+from app.models.stock import Stock
+from app.models.part_request import PartRequest
+
+from app.models.battery import Battery
+from app.models.tire import Tire
+
+from app.models.fuel_log import FuelLog
+from app.models.mission import Mission
+
+from app.models.notification import Notification

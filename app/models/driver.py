8b7ac641796ae2295fdfccd65c42f30f030a52cf
med-40 <salahbel9140@gmail.com>
2026-08-02 19:50:13 +0100

    Update driver.py

--- app/models/driver.py
+++ app/models/driver.py
@@ -10,46 +10,38 @@ class Driver(Base):
 
     __tablename__ = "drivers"
 
-    # المعرف
     id = Column(
         Integer,
         primary_key=True
     )
 
-    # الاسم
     first_name = Column(
         String(100),
         nullable=False
     )
 
-    # اللقب
     last_name = Column(
         String(100),
         nullable=False
     )
 
-    # الرتبة
     rank = Column(
         String(100)
     )
 
-    # الهاتف
     phone = Column(
         String(50)
     )
 
-    # رقم رخصة السياقة
     license_number = Column(
         String(100),
         unique=True
     )
 
-    # تاريخ انتهاء رخصة السياقة
     license_expiry_date = Column(
         Date
     )
 
-    # الحالة
     status = Column(
         String(50),
         default="Active"

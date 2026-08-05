2268d9bd5be1a22653eb09b4d8f6448ae37073d8
med-40 <salahbel9140@gmail.com>
2026-08-05 22:03:10 +0100

    Create __init__.py

---
+++ app/modules/maintenance/__init__.py
@@ -0,0 +1,13 @@
+from .models import (
+    MaintenanceOrder,
+    MaintenanceSchedule,
+    MaintenanceType,
+    MeterReading,
+)
+
+__all__ = [
+    "MaintenanceOrder",
+    "MaintenanceSchedule",
+    "MaintenanceType",
+    "MeterReading",
+]

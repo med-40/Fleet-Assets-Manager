8987276f92156887c72b147a0dd38cb1a2f6e975
med-40 <salahbel9140@gmail.com>
2026-08-06 07:40:13 +0100

    Update init_db.py

--- app/database/init_db.py
+++ app/database/init_db.py
@@ -9,6 +9,7 @@ from app.database.session import engine
 from app.models.user import User
 from app.models.role import Role
 from app.models.permission import Permission
+from app.models.notification import Notification
 
 from app.models.equipment import Equipment
 
